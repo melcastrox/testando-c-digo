adb3a103fdd289edbfeaac76c4ff1155d3920e95
Melyssa <melcastro417@gmail.com>
2025-04-03 19:16:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const pergunta1 = "Você gosta de programar?";
+const pergunta2 = "Você já estudou JavaScript antes?";
+const pergunta3 = "Você pretende praticar todos os dias?";
+
+const resposta1 = prompt(pergunta1);
+const resposta2 = prompt(pergunta2);
+const resposta3 = prompt(pergunta3);
+
+console.log(resposta1)
+console.log(resposta2)
+console.log(resposta3)
+
+
+console.log(`${pergunta1} - ${resposta1}`);
+console.log(`${pergunta2} - ${resposta2}`);
+console.log(`${pergunta3} - ${resposta3}`);
+
